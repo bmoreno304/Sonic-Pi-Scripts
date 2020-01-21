bb8ffb49b554909f5e7358ac0a9e3a3b54f9f042
bmoreno304 <45017629+bmoreno304@users.noreply.github.com>
2020-01-21 11:17:56 -0800

    Add files via upload

---
+++ custom_melody.rb
@@ -0,0 +1,123 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 100
+use_synth :piano
+play:c4
+sleep 0.5
+play:c4
+sleep 0.5
+play:g4
+sleep 0.5
+play:g4
+sleep 0.5
+play:a4
+sleep 0.5
+play:a4
+sleep 0.5
+play:g4
+sleep 0.5
+play:f4
+sleep 0.5
+play:f4
+sleep 0.5
+play:e4
+sleep 0.5
+play:e4
+sleep 0.5
+play:d4
+sleep 0.5
+play:d4
+sleep 0.5
+play:c4
+sleep 0.5
+play:g4
+sleep 0.5
+play:g4
+sleep 0.5
+play:f4
+sleep 0.5
+play:f4
+sleep 0.5
+play:e4
+sleep 0.5
+play:e4
+sleep 0.5
+play:d4
+sleep 0.5
+play:g4
+sleep 0.5
+play:g4
+sleep 0.5
+play:f4
+sleep 0.5
+play:f4
+sleep 0.5
+play:e4
+sleep 0.5
+play:e4
+sleep 0.5
+play:d4
+sleep 0.5
+play:c4
+sleep 0.5
+play:c4
+sleep 0.5
+play:g4
+sleep 0.5
+play:g4
+sleep 0.5
+play:a4
+sleep 0.5
+play:a4
+sleep 0.5
+play:g4
+sleep 0.5
+play:f4
+sleep 0.5
+play:f4
+sleep 0.5
+play:e4
+sleep 0.5
+play:e4
+sleep 0.5
+play:d4
+sleep 0.5
+play:d4
+sleep 0.5
+play:c4
+sleep 0.5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
