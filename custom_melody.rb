f3ee6ddc1a5d5dd361765f2ce9fdf3c4a4b90e66
bmoreno304 <45017629+bmoreno304@users.noreply.github.com>
2020-01-21 11:37:04 -0800

    Update custom_melody.rb

--- custom_melody.rb
+++ custom_melody.rb
@@ -1,123 +1,89 @@
 # Welcome to Sonic Pi v3.1
-use_bpm 100
-use_synth :piano
-play:c4
-sleep 0.5
-play:c4
-sleep 0.5
-play:g4
-sleep 0.5
-play:g4
-sleep 0.5
-play:a4
-sleep 0.5
-play:a4
-sleep 0.5
-play:g4
-sleep 0.5
-play:f4
-sleep 0.5
-play:f4
-sleep 0.5
-play:e4
-sleep 0.5
-play:e4
-sleep 0.5
-play:d4
-sleep 0.5
-play:d4
-sleep 0.5
-play:c4
-sleep 0.5
-play:g4
-sleep 0.5
-play:g4
-sleep 0.5
-play:f4
-sleep 0.5
-play:f4
-sleep 0.5
-play:e4
-sleep 0.5
-play:e4
-sleep 0.5
-play:d4
-sleep 0.5
-play:g4
-sleep 0.5
-play:g4
-sleep 0.5
-play:f4
-sleep 0.5
-play:f4
-sleep 0.5
-play:e4
-sleep 0.5
-play:e4
-sleep 0.5
-play:d4
-sleep 0.5
-play:c4
-sleep 0.5
-play:c4
-sleep 0.5
-play:g4
-sleep 0.5
-play:g4
-sleep 0.5
-play:a4
-sleep 0.5
-play:a4
-sleep 0.5
-play:g4
-sleep 0.5
-play:f4
-sleep 0.5
-play:f4
-sleep 0.5
-play:e4
-sleep 0.5
-play:e4
-sleep 0.5
-play:d4
-sleep 0.5
-play:d4
-sleep 0.5
-play:c4
-sleep 0.5
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+use_bpm 70
+live_loop:twinkle do
+  use_synth :piano
+  play:c4
+  sleep 0.5
+  play:c4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:a4
+  sleep 0.5
+  play:a4
+  sleep 0.5
+  play:g4
+  sleep 1.5
+  play:f4
+  sleep 0.5
+  play:f4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:d4
+  sleep 0.5
+  play:d4
+  sleep 0.5
+  play:c4
+  sleep 1.5
+  play:g4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:f4
+  sleep 0.5
+  play:f4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:d4
+  sleep 1.5
+  play:g4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:f4
+  sleep 0.5
+  play:f4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:d4
+  sleep 1.5
+  play:c4
+  sleep 0.5
+  play:c4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:g4
+  sleep 0.5
+  play:a4
+  sleep 0.5
+  play:a4
+  sleep 0.5
+  play:g4
+  sleep 1.5
+  play:f4
+  sleep 0.5
+  play:f4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:e4
+  sleep 0.5
+  play:d4
+  sleep 0.5
+  play:d4
+  sleep 0.5
+  play:c4
+  sleep 1.5
+end
